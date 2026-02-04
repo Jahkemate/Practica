6c32c73d3a2aebe66a339419ee06f6ad6befd8dc
Jahkemate <jahernandez149@unicah.edu>
2026-02-04 12:14:29 -0600

    Aprendendo Blade

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,44 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/home', function () {
+    return 'Pagina de inicio';
 });
+
+
+Route::get('/notas',function() {
+
+    $notes = [
+
+            'Nota 1: Estudiar Laravel',
+            'Nota 2: Estudiar Vue.js',
+            'Nota 3: Estudiar React',
+            'Nota 4: Estudiar Angular',
+        
+    ];
+
+    return view('notes.index')->with('notes', $notes);
+});
+
+Route::get ('/notas/{id}/', function($id) {
+    return 'Editar notas: '.$id;
+});
+
+Route::get ('/notas/{id}/editar', function($id) {
+    return 'Detalle de la nota '.$id;
+});
+
+Route::get ('/notas/crear',function() {
+    return view('notes.create');
+});
+
+Route::get ('cursos',function() {
+    return [
+        'Cursos' => [
+            'Curso de Laravel 10',
+            'Curso de programacion orintadsa a objetos',
+            "Curso de Git",
+        ]
+    ];
+});
+
